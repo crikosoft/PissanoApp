49993e191693c5324ea50f85b561d521b7458387
crikosoft <catavillos@gmail.com>
2017-05-30 11:54:17 -0500

    model

---
+++ Inspinia_MVC5_SeedProject/Models/OrdenServicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PissanoApp.Models
+{
+    public class OrdenServicio
+    {
+        public int ordenServicioId { get; set; }
+        public int materialId { get; set; }
+        public int proveedorId { get; set; }
+        public double metrado { get; set; }
+        public double precioUnitario { get; set; }
+        public double subtotal { get; set; }
+        public double igv { get; set; }
+        public double total { get; set; }
+        public int formaPagoId  { get; set; }
+        public int monedaId  { get; set; }
+        public double adelanto { get; set; }
+        public double fondoGarantia { get; set; }
+        public int MyProperty { get; set; }
+
+    }
+}
---
+++ Inspinia_MVC5_SeedProject/Models/TipoValorizacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PissanoApp.Models
+{
+    public class TipoValorizacion
+    {
+        public int tipoValorizacionId { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+    }
+}
